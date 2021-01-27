fc6067808ec315a5333850733ecb3efdf7f67290
Semburg <semburg.sb@gmail.com>
2021-01-27 15:47:43 +0100

    vers0.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
-let nameField = document.getElementById("text_").value
-console.log("Show me")
-console.log(nameField)
+
+function show_Details(){
+    let nameField = document.getElementById("text_").value
+    let mailField = document.getElementById("email_").value
+    let textField = document.getElementById("text_area_").value
+
+    console.log(nameField,)
+    console.log(mailField)
+    console.log(textField)
+}
